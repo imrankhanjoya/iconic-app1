53518f32e98ac6f7d54d976168e74648a0c125df
imran <imran.khan.joya@gmail.com>
2017-09-27 16:08:12 +0530

    page

---
+++ src/pages/askquestion/askquestion.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AskquestionPage } from './askquestion';
+
+@NgModule({
+  declarations: [
+    AskquestionPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AskquestionPage),
+  ],
+})
+export class AskquestionPageModule {}
