b7cb566843ad2ff4dcad3dd19f62c71475745024
imran <imran.khan.joya@gmail.com>
2017-10-05 12:27:44 +0530

    video tube fixed

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { VideoProvider } from '../../providers/video/video';
 import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
 
 
