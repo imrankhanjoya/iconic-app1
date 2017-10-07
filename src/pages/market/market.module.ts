c8e0e39fd0310a229b93129b02e780004fbe0d18
allanoor pathan <allanoor.pathan@gmail.com>
2017-10-07 19:22:20 +0530

    my name is khan

--- src/pages/market/market.module.ts
+++ src/pages/market/market.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MarketPage } from './market';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { MarketPage } from './market';
   ],
   imports: [
     IonicPageModule.forChild(MarketPage),
+    TranslateModule.forChild()
   ],
 })
 export class MarketPageModule {}
