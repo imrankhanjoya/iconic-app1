7c8b455163e5e08870f54b9831775f633801169b
imran <imran.khan.joya@gmail.com>
2017-10-07 15:28:51 +0530

    git commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -56,7 +56,7 @@ export class HomePage {
 
     this.geolocation.getCurrentPosition().then((resp) => {
        console.log(resp);
-       //this.storage.set('userLoction',resp.coords);
+       this.storage.set('userLoction',resp.coords);
        this.getkrish(resp.coords.latitude,resp.coords.longitude);
       }).catch((error) => {
         console.log('Error getting location', error);
