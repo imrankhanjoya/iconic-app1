c5a927d9a6a4433be91c6d998d8f9335df7d3584
allanoor pathan <allanoor.pathan@gmail.com>
2017-10-07 14:36:43 +0530

    conflict in hindi file

--- src/pages/mandi-details/mandi-details.module.ts
+++ src/pages/mandi-details/mandi-details.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MandiDetailsPage } from './mandi-details';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { MandiDetailsPage } from './mandi-details';
   ],
   imports: [
     IonicPageModule.forChild(MandiDetailsPage),
+    TranslateModule.forChild()
   ],
 })
 export class MandiDetailsPageModule {}
