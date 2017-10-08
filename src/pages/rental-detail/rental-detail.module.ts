f7ff8a56355a3475255672ea8ff878d6ed245f45
allanoor pathan <allanoor.pathan@gmail.com>
2017-10-08 13:40:31 +0530

    my name is khan

--- src/pages/rental-detail/rental-detail.module.ts
+++ src/pages/rental-detail/rental-detail.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RentalDetailPage } from './rental-detail';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,8 @@ import { RentalDetailPage } from './rental-detail';
   ],
   imports: [
     IonicPageModule.forChild(RentalDetailPage),
+    TranslateModule.forChild()
+
   ],
 })
 export class RentalDetailPageModule {}
