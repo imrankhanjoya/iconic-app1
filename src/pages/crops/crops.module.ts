38f3dbba3340ffcea7296ec9d4218b9ec514a21b
allanoor pathan <allanoor.pathan@gmail.com>
2017-10-08 15:37:43 +0530

    allanoor

--- src/pages/crops/crops.module.ts
+++ src/pages/crops/crops.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CropsPage } from './crops';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CropsPage } from './crops';
   ],
   imports: [
     IonicPageModule.forChild(CropsPage),
+    TranslateModule.forChild()
   ],
 })
 export class CropsPageModule {}
--- src/pages/select-location/select-location.module.ts
+++ src/pages/select-location/select-location.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SelectLocationPage } from './select-location';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { SelectLocationPage } from './select-location';
   ],
   imports: [
     IonicPageModule.forChild(SelectLocationPage),
+    TranslateModule.forChild()
   ],
 })
 export class SelectLocationPageModule {}
