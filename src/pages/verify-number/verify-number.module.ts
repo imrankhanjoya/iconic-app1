8c9f17a3cd17bc9bb6547933664aaeb778eb90bc
allanoor pathan <allanoor.pathan@gmail.com>
2017-10-08 15:11:25 +0530

    my name is khan

--- src/pages/verify-number/verify-number.module.ts
+++ src/pages/verify-number/verify-number.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { VerifyNumberPage } from './verify-number';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { VerifyNumberPage } from './verify-number';
   ],
   imports: [
     IonicPageModule.forChild(VerifyNumberPage),
+    TranslateModule.forChild()
   ],
 })
 export class VerifyNumberPageModule {}
