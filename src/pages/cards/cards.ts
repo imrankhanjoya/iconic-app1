649273d28c691547ea640cde362d124d69b9ea1e
islamsolnkey <islamsolnkey@gmail.com>
2017-10-15 19:00:58 +0530

    change in blog page filter

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -9,7 +9,7 @@ import { ExpertsProvider } from '../../providers/experts/experts';
 })
 export class CardsPage {
   public expertdata:{ status: string, msg: string,data: any } = {status:'false',msg: 'test',data:''};
-  
+  public catDatas: { status:string, msg: string,data: any } = {status:'false',msg: 'test',data:''};
   constructor(public navCtrl: NavController,public experts:ExpertsProvider) {
     
 
@@ -18,6 +18,7 @@ export class CardsPage {
 
     
     this.get_expert();
+    this.get_cat();
     
   }
 
@@ -29,9 +30,35 @@ export class CardsPage {
         // Unable to log in
         console.log(err);
       });
+  }
+
+  get_cat_expert(category_id){
+    this.experts.Experts_Cat_list('blogs',10,category_id).map(res => res.json()).subscribe((res) => {
+        this.expertdata = res;
+        console.log(this.expertdata);
+      }, (err) => {
+        // Unable to log in
+        console.log(err);
+      });
+  }
+
+  get_cat(){
+    this.experts.Cat_list(8).map(res => res.json()).subscribe((res) => {
+        this.catDatas.data = res.data;
+        this.catDatas.status = res.status;
+        this.catDatas.msg = res.msg;
+        console.log('all cat data');
+        console.log(res);
+      }, (err) => {
+        // Unable to log in
+        console.log(err);
+      });
     }
 
    goToExpertDetial(id){
     this.navCtrl.push('ExpertsDetailPage',{id:id}); 
+  }
+  onChange(selectedData){
+      this.get_cat_expert(selectedData);
   }
 }
--- src/providers/experts/experts.ts
+++ src/providers/experts/experts.ts
@@ -35,6 +35,45 @@ export class ExpertsProvider {
 
     return seq;
   }
+   Experts_Cat_list(post_type='blogs',limit=3,slug) {
+    //?lang=en_US&json=get_category_posts&post_type=agri_video&limit=20&slug=seeds
+      //http://205.147.100.82/agriboloapiv2/api/web/index.php?r=v1/mandi/all&page=2&state_id=12
+    var paramCond ={post_type:post_type,lang:'hi_IN',limit:limit,slug:slug};
+    let seq = this.api.get('v1/wp/cat-post', paramCond).share();
+
+    seq
+      .map(res => res.json())
+      .subscribe(res => {
+        // If the API returned a successful response, mark the user as logged in
+        if (res.status == 'success') {
+          console.log(res);
+        } else {
+        }
+      }, err => {
+        console.error('ERROR', err);
+      });
+
+    return seq;
+  }
+  Cat_list(parent_id) {
+      //http://205.147.100.82/agriboloapiv2/api/web/index.php?r=v1/mandi/all&page=2&state_id=12
+    var paramCond ={parent_id:parent_id};
+    let seq = this.api.get('v1/wp/post-cat', paramCond).share();
+
+    seq
+      .map(res => res.json())
+      .subscribe(res => {
+        // If the API returned a successful response, mark the user as logged in
+        if (res.status == 'success') {
+          console.log(res);
+        } else {
+        }
+      }, err => {
+        console.error('ERROR', err);
+      });
+
+    return seq;
+  }
 Experts_detail(expertids) {
       //http://205.147.100.82/agriboloapiv2/api/web/index.php?r=v1/mandi/all&page=2&state_id=12
       console.log(expertids+'my ost detail by noser');
