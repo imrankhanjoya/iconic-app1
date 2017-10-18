0eeed923b395c043afc9d6b40945246bd2acfddd
islamsolnkey <islamsolnkey@gmail.com>
2017-10-18 10:44:37 +0530

    change in weather filter

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -1,8 +1,8 @@
 import { Component ,ViewChild} from '@angular/core';
 import { WeatherProvider } from '../../providers/weather/weather';
 import { Storage } from '@ionic/storage';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
-import { CityStateProvider } from '../../providers/city-state/city-state';
+import { IonicPage, NavController, NavParams, LoadingController,ModalController, ViewController } from 'ionic-angular';
+import { FilterLocationPage } from '../filter-model/FilterLocationPage';
 
 /**
  * Generated class for the WeatherPage page.
@@ -20,12 +20,9 @@ export class WeatherPage {
 @ViewChild('mySlider') slider;
   public loading:any;
   public lang:any;
-  public selectState: any;
-  public stateList: any;
-  public districtList: any;
-  public tehsilList: any;
   public location:any
   public tehsilId:any;
+  public filterLocation:any;
 	public wheaterdetailall: { status:string, msg: string,data: any } = {status:'false',msg: 'test',data:''};
   	public weatherfiveday: { status:string, msg: string,data: any } = {status:'false',msg: 'test',data:''};
   	constructor(
@@ -34,18 +31,22 @@ export class WeatherPage {
               public loadingCtrl: LoadingController,
               public storage:Storage,
               public weather:WeatherProvider,
-              public cityStateProvider:CityStateProvider
+              public modalCtrl:ModalController,
+              public viewCtrl:ViewController
               ) {
+
+              this.tehsilId = navParams.get('filter_tehsil');
       }
 
   ionViewDidLoad() {
-    this.getAllState();
     this.weatherdetail(1);
     this.weatherfivedays(1);
     console.log('ionViewDidLoad WeatherPage');
   }
 
  weatherdetail(tehsil){
+
+        var tehsil = ( typeof this.tehsilId != 'undefined' )?this.tehsilId:tehsil;
         this.loading = this.loadingCtrl.create({
           content: 'Please wait...'
         });
@@ -82,40 +83,23 @@ export class WeatherPage {
         console.log(err);
       });
   }
-
-  onStateSelect(stateid) {
-    var array = stateid.split('~');
-    this.cityStateProvider.getDistrict(this.lang,array[0]).map(res => res.json()).subscribe((resp) => {
-        this.districtList=resp.data;
-        console.log('-----tehId---------'+this.tehsilId);
-        console.log('this is district');
-        console.log(this.districtList);
-      });
-  }
-  onDistrictSelect(districtId){
-    var array = districtId.split('~');
-    this.storage.set('userDictrictId',array[0]);
-    this.storage.set('userDictrict',array[1]);this.cityStateProvider.getTehsil(this.lang,array[0]).map(res => res.json()).subscribe((resp) => {
-      this.tehsilList=resp.data;
-      console.log('+++++tehsilId+++'+JSON.stringify(resp));
-      //  this.loading.dismiss();
-    });
-  }
-
-  getAllState() {
-    this.cityStateProvider.getState(this.lang).map(res => res.json()).subscribe((resp) => {
-      this.stateList=resp.data;
-      console.log(this.stateList);
-      this.loading.dismiss();
-    });
-  }
+  
   weatherget(tehsil){
     this.weatherdetail(tehsil);
     this.weatherfivedays(tehsil);
   }
-slideChanged(index){
-console.log(index);
-}
+  slideChanged(index){
+    console.log(index);
+  }
+  openFilter(){
+    let modal = this.modalCtrl.create('FilterLocationPage');
+    modal.present();
+  }
+  
+  dismiss(){
+        let data = { 'foo': 'bar' };
+        this.viewCtrl.dismiss(data);
+  }
 
 
 
