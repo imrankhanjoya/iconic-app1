b81231f2ad5e75140bd3101372167ff4daa10628
imran <imran.khan.joya@gmail.com>
2017-11-04 17:57:52 +0530

    se

---
+++ src/providers/search/search.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import { Api } from '../api/api';
+
+/*
+  Generated class for the SearchProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SearchProvider {
+
+  constructor(public http: Http, public api: Api) {
+    console.log('Hello SearchProvider Provider');
+  }
+
+
+  find(keyword){
+		var paramCond ={keyword:keyword};
+	    return new Promise((resolve)=>{
+	      this.api.getCache('v1/search/find', paramCond).then((searchItem)=>{
+	        resolve(searchItem);
+	      });  
+	    });
+		
+  }
+
+
+}
