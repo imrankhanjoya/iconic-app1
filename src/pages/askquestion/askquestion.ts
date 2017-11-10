1aade7bba9e2bcca1c0d53f77844afaaff5f7a23
islamsolnkey <islamsolnkey@gmail.com>
2017-11-10 17:46:54 +0530

    cahnge in backened file

--- src/pages/askquestion/askquestion.ts
+++ src/pages/askquestion/askquestion.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, PopoverController, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController, ViewController, ToastController} from 'ionic-angular';
 import { QuestionsProvider } from '../../providers/questions/questions';
 import { Storage } from '@ionic/storage';
 import { Camera, CameraOptions } from '@ionic-native/camera';
@@ -18,18 +18,35 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class AskquestionPage {
   private user_id :any;
+  private image :any;
   public  questionaddData = {user_id:'',title:'',description:'',privacy:'',Attachments:''};
 	public askquestionsData: { status:boolean, msg: string,data: any } = {status:false,msg: 'test',data:''};
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public viewCtrl: ViewController,
               public QuestionsProvider: QuestionsProvider, public camera:Camera,
-              public storage:Storage, public popoverCtrl: PopoverController,) {
+              public storage:Storage, public popoverCtrl: PopoverController,
+              private toastCtrl: ToastController
+              ) {
 
                 this.storage.get('userData').then((val) => {
                   this.user_id = val.ID; 
                 });
               }
+
+            presentToast(message) {
+              let toast = this.toastCtrl.create({
+                message: message,
+                duration: 3000,
+                position: 'bottom'
+              });
+
+              toast.onDidDismiss(() => {
+                console.log('Dismissed toast');
+              });
+
+              toast.present();
+            }
 
   ionViewDidLoad() {
     //this.getaskquestions();
@@ -40,13 +57,18 @@ export class AskquestionPage {
 
     this.QuestionsProvider.askquestion(this.user_id,this.questionaddData).map(res => res.json()).subscribe((res) => {
       
-        /*this.askquestionsData.data = res.data;
         this.askquestionsData.msg = res.msg;
-        this.askquestionsData.status = res.status;*/
-        
-        this.viewCtrl.dismiss();
-        console.log(this.askquestionsData.data);
-         this.navCtrl.push('QuestionlistPage');
+        this.askquestionsData.status = res.status;
+
+        if (res.status==false) {
+            this.presentToast('Please Enter Question');
+        }else{
+
+          this.viewCtrl.dismiss();
+          this.presentToast('Questions added successfully');
+          this.navCtrl.push('QuestionlistPage');
+          console.log(this.askquestionsData.data);
+        }
       }, (err) => {
         // Unable to log in
         console.log(err);
@@ -70,6 +92,7 @@ export class AskquestionPage {
              destinationType: this.camera.DestinationType.DATA_URL
             }).then((imageData) => {
               console.log('=========data:image/jpeg;base64,'+imageData);
+              this.image = 'data:image/jpeg;base64,'+imageData;
              this.questionaddData.Attachments=imageData;
              }, (err) => {
               console.log(err);
@@ -81,6 +104,7 @@ export class AskquestionPage {
              destinationType: this.camera.DestinationType.DATA_URL
             }).then((imageData) => {
               console.log('=========data:image/jpeg;base64,'+imageData);
+              this.image = 'data:image/jpeg;base64,'+imageData;
               this.questionaddData.Attachments=imageData;
              }, (err) => {
               console.log(err);
