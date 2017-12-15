332dc11a8b06658289f0835e555095aaa645895b
allanoor pathan <allanoor.pathan@gmail.com>
2017-12-15 12:44:41 +0530

    now change

--- src/pages/experts-detail/experts-detail.ts
+++ src/pages/experts-detail/experts-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController,AlertController } from 'ionic-angular';
 import { ExpertsProvider } from '../../providers/experts/experts';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * Generated class for the ExpertsDetailPage page.
@@ -16,10 +17,25 @@ import { ExpertsProvider } from '../../providers/experts/experts';
 })
 export class ExpertsDetailPage {
   public expertid:any;
+  public aniName:any;
   public loading:any;
   public expertdetail:{ status: string, msg: string,data: any } = {status:'false',msg: 'test',data:''};
-  constructor(public navCtrl: NavController, public navParams: NavParams,public experts:ExpertsProvider,
-    public loadingCtrl: LoadingController) {
+  constructor(public translateService: TranslateService,public navCtrl: NavController, public navParams: NavParams,public experts:ExpertsProvider,
+    public loadingCtrl: LoadingController,private alertCtrl: AlertController) {
+
+    this.translateService.get('CANCEL_BUTTON').subscribe((value) => {
+      this.CANCEL_BUTTON= value;
+    });
+    this.translateService.get('CALL').subscribe((value) => {
+      this.CALL= value;
+    });
+    this.translateService.get('CALL_TOLLFREE').subscribe((value) => {
+      this.CALL_TOLLFREE= value;
+    });
+    
+    this.textSlide='';
+    this.buttonOnCloseCSS='';
+    
     this.expertid=navParams.get('id');
     this.loading = this.loadingCtrl.create({
         content: 'Please wait...'
@@ -46,5 +62,46 @@ export class ExpertsDetailPage {
       });
 
   }
+   mackCall(){
+    this.presentConfirm();
+    console.log('-----------------------------');
+  }
+  startAnimitio(){
+      this.buttonOnCloseCSS="buttonOnClose";
+      this.aniName="openCallButton";
+      this.textSlide="fadeInLeftMarket"
+      setTimeout(() => {
+        this.textSlide="textGoingBack"
+      }, 3000);
+
+      setTimeout(() => {
+        this.aniName="closeCallButton"
+      }, 4000);
+  }
+
+   presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirm purchase',
+      message: this.CALL_TOLLFREE,
+      buttons: [
+        {
+          text: this.CANCEL_BUTTON,
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clikkkcked');
+          }
+        },
+        {
+          text: this.CALL,
+          handler: () => {
+            window.location.href = "tel:18001200800";
+            console.log('Buy clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
 }
