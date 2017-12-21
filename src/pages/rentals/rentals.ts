8493eb47fead9c3c87d0cfbc19df9f3045e0a001
allanoor pathan <allanoor.pathan@gmail.com>
2017-12-21 19:58:18 +0530

    change

--- src/pages/rentals/rentals.ts
+++ src/pages/rentals/rentals.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,LoadingController,AlertController } from 'ionic-angular';
 import { RentalsProvider } from '../../providers/rentals/rentals';
 import { Events } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+
 
 /**
  * Generated class for the RentalsPage page.
@@ -19,20 +21,57 @@ export class RentalsPage {
   public items:any = [];
   private page:number=0;
   public Rental_Listdata: { status:boolean, msg: string,data: any } = {status:false,msg: 'test',data:''};
-  constructor(public navCtrl: NavController, public navParams: NavParams,public events: Events,
-public rentals:RentalsProvider,public loadingCtrl:LoadingController ) 
+  constructor( public translateService: TranslateService,public navCtrl: NavController, public navParams: NavParams,public events: Events,
+public rentals:RentalsProvider,private alertCtrl: AlertController,public loadingCtrl:LoadingController ) 
   {
     this.loading = this.loadingCtrl.create({
         content: 'Please wait...'
       });
       this.loading.present();
 
+       this.translateService.get('CANCEL_BUTTON').subscribe((value) => {
+      this.CANCEL_BUTTON= value;
+    });
+    this.translateService.get('CALL').subscribe((value) => {
+      this.CALL= value;
+    });
+    this.translateService.get('CALL_TOLLFREE').subscribe((value) => {
+      this.CALL_TOLLFREE= value;
+    });
   }
 
   ionViewDidLoad() {
     this.getRental();
     console.log('ionViewDidLoad RentalsPage');
   }
+   mackCall(){
+    this.presentConfirm();
+    console.log('-----------------------------');
+  }
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirm purchase',
+      message: this.CALL_TOLLFREE,
+      buttons: [
+        {
+          text: this.CANCEL_BUTTON,
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clikkkcked');
+          }
+        },
+        {
+          text: this.CALL,
+          handler: () => {
+            window.location.href = "tel:18001200800";
+            console.log('Buy clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
   getRental(){
     this.rentals.Rental_list(this.page,10).then((res)=>{
